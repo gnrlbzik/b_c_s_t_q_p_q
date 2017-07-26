b1c5a2dd1ee2b3da2d6f9ed8e175e7f3bf8cdca0
Alesei N <gnrlbzik@users.noreply.github.com>
2017-07-26 13:38:54 -0500

    Update maximum-sum-subarray-problem.js

--- maximum-sum-subarray-problem/maximum-sum-subarray-problem.js
+++ maximum-sum-subarray-problem/maximum-sum-subarray-problem.js
@@ -1,4 +1,12 @@
-/*  First JavaScript Implementation 
+/*  WIP: JavaScript Implementation as I understand it now, probabaly wrong. 
+
+    Next steps:
+        * understand question better:
+            - http://www.geeksforgeeks.org/largest-sum-contiguous-subarray/
+            - https://stackoverflow.com/questions/32367032/kadanes-algorithm-explained
+        * redo if need to
+
+
     view code: https://codepad.remoteinterview.io/PHBVDAUEPG
     
     The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers:
@@ -10,9 +18,11 @@
 
     Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
     
-START */
 
-function maxSequence (sequence) {
+    
+/* START *//* verstion 1.0.0 */
+
+function maxSequenceV1 (sequence) {
     
     let sum = 0, tempSum = 0;
     
@@ -34,6 +44,8 @@ function maxSequence (sequence) {
     
 }
 
+/* END *//* verstion 1.0.0 */
+
 function assertEquals (testValue, expectedResult) {
     if (testValue === expectedResult) {
         console.log('SUCCESS: ' + testValue + ' equals ' + expectedResult);
@@ -42,6 +54,6 @@ function assertEquals (testValue, expectedResult) {
     }
 }
 
-assertEquals(maxSequence([]), 0);
-assertEquals(maxSequence([-1, -2, -3]), 0);
-assertEquals(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]), 6);
+assertEquals(maxSequenceV1([]), 0);
+assertEquals(maxSequenceV1([-1, -2, -3]), 0);
+assertEquals(maxSequenceV1([-2, 1, -3, 4, -1, 2, 1, -5, 4]), 6);
